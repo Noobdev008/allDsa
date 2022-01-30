cfe5c48392b33ad67250a09154f4e3440cd085d9
Shubham Srivastava <86356397+Noobdev008@users.noreply.github.com>
2022-01-30 23:41:32 +0530

    Add files via upload

---
+++ DSA/leetCode/mergeSort.js
@@ -0,0 +1,36 @@
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+ 
+
+// Example 1:
+
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+// Example 2:
+
+// Input: nums1 = [1], m = 1, nums2 = [], n = 0
+// Output: [1]
+// Explanation: The arrays we are merging are [1] and [].
+// The result of the merge is [1].
+
+
+
+ var merge = function(nums1, m, nums2, n) {
+    var a1 = m-1;
+    var a2 = n-1;
+    var i = m+n-1;
+    while(a2>=0){
+        if(a1>=0 && nums1[a1]>nums2[a2]){
+            nums1[i--]=nums1[a1--]
+        }else{
+            nums1[i--]=nums2[a2--]
+        }
+    }
+    return nums1
+};
